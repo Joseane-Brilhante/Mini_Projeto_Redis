647bf1ee0e91b25be9ff357a2aaf9a3be30e3ca1
Joseane-Brilhante <joseane.brilhante@academico.ifpb.edu.br>
2022-02-19 23:07:54 -0300

    Excluindo node modules

---
+++ api/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const userCrud = require('./database/userCrud')
+const redis = require('./database/redis')
+const mongodb = require('./database/mongo')
+
+app.get('/inserir/:nome/cpf/:cpf/email/:email/senha/:senha', userCrud.inserirUsuario);
+app.get('/visualizar/:cpf', userCrud.visualizarUsuario);
+app.get('/atualizar/:cpf/nome/:nome/email/:email/senha/:senha',userCrud.atualizarUsuario);
+app.get('/excluir/:cpf',userCrud.deletarUsuario);
+
+app.get('/salvarRascunho/:cpf/rascunho/:rascunho',redis.salvarRascunho);
+app.get('/lerRascunho/:cpf',redis.lerRascunho);
+app.get('/deletarRascunho/:cpf',redis.deletarRascunho);
+
+app.get('/salvarPost/:titulo/texto/:texto/cpf/:cpf',mongodb.salvarPost);
+app.get('/obterPosts/:cpf',mongodb.obterPosts);
+
+app.listen(process.env.PORT, () => { 
+  console.log(`Server listening on port ${process.env.PORT}`); 
+});
